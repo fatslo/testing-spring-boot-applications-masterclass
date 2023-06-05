c350a40a2c930e0811d0c4f8480cd0ded1d48389
fatslow <fatslow@web.de>
2023-06-05 21:54:07 +0200

    Writing tests with the whole spring context - First try to start the whole context

--- src/test/java/de/rieckpil/courses/book/management/BookSynchronizationListenerIT.java
+++ src/test/java/de/rieckpil/courses/book/management/BookSynchronizationListenerIT.java
@@ -5,6 +5,7 @@ import de.rieckpil.courses.initializer.RSAKeyGenerator;
 import de.rieckpil.courses.initializer.WireMockInitializer;
 import de.rieckpil.courses.stubs.OAuth2Stubs;
 import de.rieckpil.courses.stubs.OpenLibraryStubs;
+import io.awspring.cloud.sqs.operations.SqsTemplate;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,9 +20,12 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.localstack.LocalStackContainer;
 import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.utility.DockerImageName;
 
 import java.io.IOException;
 import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 @ActiveProfiles("integration-test")
@@ -29,19 +33,19 @@ class BookSynchronizationListenerIT {
 
   @Container
   static PostgreSQLContainer database =
-    (PostgreSQLContainer) new PostgreSQLContainer("postgres:12")
+    (PostgreSQLContainer) new PostgreSQLContainer<>("postgres:15.3")
       .withDatabaseName("test")
       .withUsername("duke")
       .withPassword("s3cret")
-      .withReuse(true);
+//      .withReuse(true);
   ;
 
   @Container
   static LocalStackContainer localStack =
-    (LocalStackContainer) new LocalStackContainer("0.10.0")
+    new LocalStackContainer(DockerImageName.parse("localstack/localstack:2.0.2"))
       .withServices(LocalStackContainer.Service.SQS)
-      .withEnv("DEFAULT_REGION", "eu-central-1")
-      .withReuse(true)
+//      .withEnv("DEFAULT_REGION", "eu-central-1")
+//      .withReuse(true)
     ;
 
   private static final String QUEUE_NAME = UUID.randomUUID().toString();
@@ -51,39 +55,49 @@ class BookSynchronizationListenerIT {
     registry.add("spring.datasource.password", database::getPassword);
     registry.add("spring.datasource.username", database::getUsername);
     registry.add("sqs.book-synchronization-queue", () -> QUEUE_NAME);
-  }
-  @TestConfiguration
-  static class TestConfig {
-    @Bean
-    public AmazonSQS amazonSQSAsync() {
-      return Amazon
-    }
+    registry.add("spring.cloud.aws.credentials.secret-key", () -> "foo");
+    registry.add("spring.cloud.aws.credentials.access-key", () -> "bar");
+    registry.add("spring.cloud.aws.endpoint", () -> localStack.getEndpointOverride(LocalStackContainer.Service.SQS));
+
   }
 
   @BeforeAll
   static void beforeAll() throws IOException, InterruptedException {
     localStack.execInContainer("awslocal", "sqs", "create-queue", "--queue-name", QUEUE_NAME);
   }
-  @Autowired
-  private WebTestClient webTestClient;
 
-  @Autowired
-  private RSAKeyGenerator rsaKeyGenerator;
-
-  @Autowired
-  private OAuth2Stubs oAuth2Stubs;
-
-  @Autowired
-  private OpenLibraryStubs openLibraryStubs;
-
-  @Autowired
-  private BookRepository bookRepository;
-
-  @Test
-  void shouldGetSuccessWhenClientIsAuthenticated() throws JOSEException {
+  static {
+    database.start();
+    localStack.start();
   }
 
+  @Autowired
+  private SqsTemplate sqsTemplate;
+
+//  @Autowired
+//  private WebTestClient webTestClient;
+//
+//  @Autowired
+//  private RSAKeyGenerator rsaKeyGenerator;
+//
+//  @Autowired
+//  private OAuth2Stubs oAuth2Stubs;
+//
+//  @Autowired
+//  private OpenLibraryStubs openLibraryStubs;
+//
+//  @Autowired
+//  private BookRepository bookRepository;
+
   @Test
-  void shouldReturnBookFromAPIWhenApplicationConsumesNewSyncRequest() {
+  void shouldStartContextWhenAllInfrastructureIsAvailable() {
+    assertNotNull(sqsTemplate);
   }
+//  @Test
+//  void shouldGetSuccessWhenClientIsAuthenticated() throws JOSEException {
+//  }
+//
+//  @Test
+//  void shouldReturnBookFromAPIWhenApplicationConsumesNewSyncRequest() {
+//  }
 }
