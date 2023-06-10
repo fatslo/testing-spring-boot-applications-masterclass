31054fecbd574c0c68bb6590b2c3896ae4b88e21
fatslow <fatslow@web.de>
2023-06-10 12:12:44 +0200

    continued to ensure users can create reviews

--- src/test/java/de/rieckpil/courses/book/management/BookSynchronizationListenerRefactoredIT.java
+++ src/test/java/de/rieckpil/courses/book/management/BookSynchronizationListenerRefactoredIT.java
@@ -1,7 +1,15 @@
 package de.rieckpil.courses.book.management;
 
 import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JOSEObjectType;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.crypto.RSASSASigner;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
 import de.rieckpil.courses.AbstractIntegrationTest;
+import de.rieckpil.courses.initializer.RSAKeyGenerator;
+import io.awspring.cloud.sqs.operations.SqsTemplate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,23 +19,85 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.io.IOException;
 import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 import static org.awaitility.Awaitility.given;
 
 class BookSynchronizationListenerRefactoredIT extends AbstractIntegrationTest {
 
-  @Autowired
-  private WebTestClient webTestClient;
+  private static final String ISBN = "9780596004651";
+  private static String VALID_RESPONSE;
 
-  @Autowired
-  private BookRepository bookRepository;
+  static {
+    try {
+      VALID_RESPONSE =
+        new String(
+          BookSynchronizationListenerRefactoredIT.class
+            .getClassLoader()
+            .getResourceAsStream("stubs/openlibrary/success-" + ISBN + ".json")
+            .readAllBytes());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Autowired private SqsTemplate sqsTemplate;
+
+  @Autowired private WebTestClient webTestClient;
+
+  @Autowired private BookRepository bookRepository;
 
   @Test
   void shouldGetSuccessWhenClientIsAuthenticated() throws JOSEException {
+    // Mache Request zu geschuetztem Endpoint und vergewissere dass mit authentifiziertem
+    // Benutzer, also gueltigem authorization-token im Header, eine 200er Antwort
+    // vom Server kommt
+    webTestClient
+      .get()
+      .uri("/api/books/reviews/statistics")
+      .header(HttpHeaders.AUTHORIZATION, "Bearer " + getSignedJWT())
+      .exchange()
+      .expectStatus().is2xxSuccessful()
+    ;
   }
 
   @Test
   void shouldReturnBookFromAPIWhenApplicationConsumesNewSyncRequest() {
+
+    webTestClient
+      .get()
+      .uri("/api/books")
+      .exchange()
+      .expectStatus().isOk()
+      .expectBody().jsonPath("$.size()").isEqualTo(0)
+    ;
+
+    this.openLibraryStubs.stubForSuccessfulBookResponse(ISBN, VALID_RESPONSE);
+
+    this.sqsTemplate.send(
+      QUEUE_NAME,
+      """
+          {
+            "isbn": "%s"
+          }
+        """.formatted(ISBN));
+
+    given()
+      .atMost(Duration.ofSeconds(5))
+      .await()
+      .untilAsserted(() -> {
+        webTestClient
+          .get()
+          .uri("/api/books")
+          .exchange()
+          .expectStatus().isOk()
+          .expectBody()
+          .jsonPath("$.size()").isEqualTo(1)
+          .jsonPath("$[0].isbn").isEqualTo(ISBN)
+        ;
+      });
   }
 }
--- src/test/java/de/rieckpil/courses/book/review/ReviewControllerIT.java
+++ src/test/java/de/rieckpil/courses/book/review/ReviewControllerIT.java
@@ -4,6 +4,7 @@ import com.nimbusds.jose.JOSEException;
 import de.rieckpil.courses.AbstractIntegrationTest;
 import de.rieckpil.courses.book.management.Book;
 import de.rieckpil.courses.book.management.BookRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,8 +23,74 @@ class ReviewControllerIT extends AbstractIntegrationTest {
   @Autowired
   private BookRepository bookRepository;
 
+  @Autowired
+  private ReviewRepository reviewRepository;
+
+  @BeforeEach
+  void beforeEach() {
+    // es reicht, lediglich das Buch fuer den Review zu speichern, der gesamte Prozess
+    // mit Import des Buchs ist bereits an anderer Stelle getestet.
+    Book book = new Book();
+    book.setPublisher("Duke Inc.");
+    book.setIsbn(ISBN);
+    book.setPages(42L);
+    book.setTitle("Joyful testing with Spring Boot");
+    book.setDescription("Writing unit and integration tests for Spring Boot applications");
+    book.setAuthor("rieckpil");
+    book.setThumbnailUrl(
+      "https://rieckpil.de/wp-content/uploads/2020/08/tsbam_introduction_thumbnail-585x329.png.webp");
+    book.setGenre("Software Development");
+
+    this.bookRepository.save(book);
+  }
+  @AfterEach
+  void afterEach() {
+    this.reviewRepository.deleteAll();
+    this.bookRepository.deleteAll();
+  }
+
   @Test
   void shouldReturnCreatedReviewWhenBookExistsAndReviewHasGoodQuality() throws JOSEException {
+    String reviewPayload =
+      """
+    {
+      "reviewTitle" : "Great book with lots of tips & tricks",
+      "reviewContent" : "I can really recommend reading this book. It includes up-to-date library versions and real-world examples",
+      "rating": 4
+    }
+    """;
+
+    String validJWT = getSignedJWT();
+
+    HttpHeaders responseHeaders =
+      this.webTestClient
+        .post()
+        .uri("/api/books/{isbn}/reviews", ISBN)
+        .header(HttpHeaders.AUTHORIZATION, "Bearer " + validJWT)
+        .contentType(MediaType.APPLICATION_JSON) //TODO Was passiert ohne?
+        .bodyValue(reviewPayload)
+        .exchange()
+        .expectStatus()
+        .isCreated()
+        .returnResult(ResponseEntity.class)
+        .getResponseHeaders();
+
+    System.out.println("responseHeaders.getLocation() = " + responseHeaders.getLocation());
+    this.webTestClient
+      .get()
+      .uri(responseHeaders.getLocation())
+      .header(HttpHeaders.AUTHORIZATION, "Bearer " + validJWT)
+      .exchange()
+      .expectStatus()
+      .isOk()
+      .expectBody()
+      .jsonPath("$.reviewTitle")
+      .isEqualTo("Great book with lots of tips & tricks")
+      .jsonPath("$.rating")
+      .isEqualTo(4)
+      .jsonPath("$.bookIsbn")
+      .isEqualTo(ISBN);
+
   }
 
   @Test
